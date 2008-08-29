8a86a0d5d849a7bea8ab4fc3869f57e83a2526f2
Mike Brasher <mike@brashersoftlab.com>
2008-08-29 00:55:28 +0000

    Development helper script to register and copy everything into place.

---
+++ register.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+cp ./build/swig/python/cmpi.py ./swig/python/pycmpi_provider.py /usr/lib64/python2.5/site-packages
+cp ./mof/Py_UnixProcess.reg /var/lib/sfcb/stage/regs
+cp ./mof/Py_UnixProcess.mof /var/lib/sfcb/stage/mofs/root/cimv2
+cp ./build/swig/python/libpyCmpiProvider.so /usr/lib64/
+cp ./swig/python/Py_UnixProcessProvider.py /usr/lib/pycim
+sfcbrepos -f
