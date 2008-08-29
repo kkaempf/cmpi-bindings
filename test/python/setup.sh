3d982ee5a3d6f25dc6942f1a5fd5cd007c4fa7bd
Mike Brasher <mike@brashersoftlab.com>
2008-08-29 23:46:27 +0000

    New files to setup providers registraiton etc.

---
+++ test/python/setup.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+function __install {
+
+    fn=$1
+    dir=$2
+
+    if [ ! -f "$fn" ]; then
+        echo "no such file: $fn"
+        exit 1
+    fi
+
+    if [ ! -d "$dir" ]; then
+        echo "no such directory: $dir"
+        exit 1
+    fi
+
+    rm -f $dir/$fn
+    ln -s $fn $dir
+}
+
+__install TestMethod.mof /var/lib/sfcb/stage/mofs/root/cimv2
+__install TestAssoc.mof /var/lib/sfcb/stage/mofs/root/cimv2
+__install TestAtom.mof /var/lib/sfcb/stage/mofs/root/cimv2
+
+__install TestMethod.py /usr/lib/pycim
+__install TestAssocProvider.py /usr/lib/pycim
+__install TestAtomProvider.py /usr/lib/pycim
+
+__install TestAssocProvider.sfcb.reg /var/lib/sfcb/stage/regs
+__install TestMethod.sfcb.reg /var/lib/sfcb/stage/regs
+__install TestAtomProvider.sfcb.reg /var/lib/sfcb/stage/regs
+
+sfcbrepos -f
