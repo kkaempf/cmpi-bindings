55e9edf694ad09800476c665cd283373496c128c
Klaus Kämpf <kkaempf@suse.de>
2010-09-14 17:09:49 +0200

    The provider constructor gets the miName, not the ClassName

--- swig/ruby/cmpi.rb
+++ swig/ruby/cmpi.rb
@@ -4,35 +4,39 @@
 # Main entry point for cmpi-bindings-ruby, Ruby based CIM Providers
 #
 
+class String
+  def decamelize
+    # CamelCase -> under_score
+    self.gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+	 gsub(/([a-z\d])([A-Z])/,'\1_\2').
+	 tr("-", "_").
+	 downcase
+  end
+end
+
 module Cmpi
   # init
   
   #
   # on-demand loading of Ruby providers
-  # create provider for 'classname'
+  # create provider for 'miname'
   #  pass 'broker' to its constructor
   #
-  def self.create_provider classname, broker, context
+  def self.create_provider miname, broker, context
 
     context.each do |name,value|
       STDERR.puts "Context '#{name}' = #{value}"
     end
-    # CamelCase -> under_score
-    underscore = classname.
-	           gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
-		   gsub(/([a-z\d])([A-Z])/,'\1_\2').
-		   tr("-", "_").
-		   downcase
 
     begin
       # Expect provider below cmpi/providers
-      require File.join(File.dirname(__FILE__),"cmpi","providers",underscore) # add to load path
+      require File.join(File.dirname(__FILE__),"cmpi","providers",miname.decamelize) # add to load path
     rescue Exception
-      STDERR.puts "Loading provider #{underscore}.rb for class #{classname} failed: #{$!.message}"
+      STDERR.puts "Loading provider #{miname.decamelize}.rb for provider #{miname} failed: #{$!.message}"
       raise
     end
 
-    Cmpi.const_get(classname).new broker
+    Cmpi.const_get(miname).new broker
   end
 
   class CMPIContext
